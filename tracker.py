2301f29459512559c3ba9274d882b11d12d47906
Braden <bradenoptimus@gmail.com>
2023-11-15 19:49:57 -0700

    ehh

--- tracker.py
+++ tracker.py
@@ -1,12 +1,44 @@
 from tkinter import *
 import json
+import os.path
+import datetime as dt
 
-# Opening JSON file
-with open('tracking.json', 'r') as openfile:
-    json_object = json.load(openfile)
+'''
+Todos:
+- 
+'''
+tracker_made = os.path.isfile('./tracking.json')
+tracker = {}
+is_new = False
 
-root = Tk()
-root.title("Habit Tracker")
-root.geometry("640x360")
+if not tracker_made:
+    unprep = Tk()
+    unprep.title("Habit Tracker")
+    label = Label(
+        unprep,
+        '''
+        Error: tracking.json file expected.
+        In order to use the tracker, please create a tracking.json
+        file in the same directory as the tracker.py script
+        and give it the following fields:
+        - "habit title"
+        - "start"
+        - "end"
+        - "goal"
+        - "period"
+        - "total"
+        - "streak"
+        - "last"
+        '''
+        )
+    label.pack()
+    unprep.geometry("640x360")
+    unprep.mainloop()
+else:
+    with open('tracking.json', 'r') as openfile:
+            tracker = json.load(openfile)
 
-root.mainloop()
+    creating = Tk()
+    creating.title("Habit Tracker")
+    creating.geometry("640x360")
+    creating.mainloop()
